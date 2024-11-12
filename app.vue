04d032bf9bbdf424653b16f61e02e84f6f78b5c9
Kristian Binau <kristian@binau.me>
2024-11-12 12:41:25 +0100

    Dynamic view height

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col items-center bg-gray-100 dark:bg-gray-900 min-h-screen"
+    class="flex flex-col items-center bg-gray-100 dark:bg-gray-900 min-h-dvh"
   >
     <NuxtRouteAnnouncer />
     <header class="md:container md:px-0 sm:px-3 px-1 w-full">

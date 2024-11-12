bd7bef7035af11966cf690bfc30d7bf785f01989
Kristian Binau <kristian@binau.me>
2024-11-12 23:03:11 +0100

    Setup wrangler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-09-19",
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/content'],
   ui: {
     global: true
   },
   nitro: {
+    preset: 'cloudflare_module',
     prerender: {
       autoSubfolderIndex: false
     }

2e6271d2e7cd6a4f518b63a628523692a5d90de3
Kristian Binau <kristian@binau.me>
2024-11-13 21:29:02 +0100

    Try NuxtHub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-09-19",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/content'],
+  modules: ['@nuxt/ui', '@nuxt/content', '@nuxthub/core'],
   ui: {
     global: true
   },
-  nitro: {
-    preset: 'cloudflare_module',
-    prerender: {
-      autoSubfolderIndex: false
-    }
-  }
 })

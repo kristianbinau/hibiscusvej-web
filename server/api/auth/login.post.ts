1360aba9c1faa5cdbffbc33d3e62393b5dc916de
Kristian Binau <kristian@binau.me>
2024-11-15 15:44:45 +0100

    Login Tokens & User endpoint working

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,17 +1,38 @@
-export default eventHandler(async () => {
+export default eventHandler(async (event) => {
     const users = await useDrizzle().select().from(tables.users).all()
 
     // TODO - Actually implement login logic
 
     const user = users[0] // Logged in user
 
-    // TODO - Implement JWT tokens generation both accessToken and refreshToken
+    // JWT tokens generation both accessToken and refreshToken
+    const { refreshToken, accessToken } = await generateTokens(user, null)
+    const decodedRefreshToken = await verifyToken(refreshToken) as { payload: { exp: number, jti: string } };
 
-    // TODO - Add refreshToken to UserSession table
+    console.log('refreshToken:', refreshToken)
+    console.log('payload:', decodedRefreshToken)
 
-    // TODO - Send refreshToken as a cookie
+    // Add refreshToken to UserSession table
+    await useDrizzle().insert(tables.userSessions).values({
+        userId: user.id,
+        refreshToken: refreshToken,
+        tokenFamily: decodedRefreshToken.payload.jti,
+        expiredAt: new Date(decodedRefreshToken.payload.exp * 1000),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    }).get()
 
-    // TODO - Return accessToken
+    // Send refreshToken as a cookie
+    setCookie(event, 'REFRESH-TOKEN', refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        path: '/api/auth/refresh',
+        maxAge: decodedRefreshToken.payload.exp - Math.floor(Date.now() / 1000),
+    })
 
-    return user
+    // Return accessToken
+    return {
+        accessToken: accessToken,
+    }
 })
--- server/api/auth/user.get.ts
+++ server/api/auth/user.get.ts
@@ -1,3 +1,27 @@
-export default eventHandler(async () => {
-    return 'Not implemented';
+export default eventHandler(async (event) => {
+    const authHeader = getRequestHeader(event, 'Authorization')
+
+    if (!authHeader) {
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'Unauthorized',
+        })
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decodedToken = await verifyToken(token)
+
+        return {
+            user: {
+                id: decodedToken.payload.sub,
+            },
+        }
+    } catch (error) {
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'Unauthorized',
+        })
+    }
 })

37b291c500d6920ee6f91cdbfa8a5570af35eed0
Kristian Binau <kristian@binau.me>
2024-11-22 15:03:04 +0100

    Began work on booking

--- pages/u/communal/book.vue
+++ pages/u/communal/book.vue
@@ -1,16 +1,28 @@
 <template>
-  <div>
-    <h1>Book</h1>
-  </div>
+	<div>
+		<h1>Book</h1>
+    <DatePicker v-model="date" :disabled-dates="disabledDates" mode="date" is-dark="system" borderless transparent expanded/>
+	</div>
 </template>
 
 <script lang="ts" setup>
+import { Calendar, DatePicker } from 'v-calendar';
+import 'v-calendar/style.css';
+
 definePageMeta({
 	layout: 'logged-in',
 	middleware: 'auth-required',
 });
+
+const date = ref(new Date());
+
+const disabledDates = ref([
+  {
+    repeat: {
+      weekdays: [4, 5],
+    },
+  },
+]);
 </script>
 
-<style>
-
-</style>
+<style></style>

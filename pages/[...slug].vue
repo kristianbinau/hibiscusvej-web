d56d0ecc014f90138bca7e1960422e2f06efb8dc
Kristian Binau <kristian@binau.me>
2025-02-14 23:10:42 +0100

    Fix 404, on [...slug]

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -9,4 +9,11 @@ const route = useRoute();
 const { data: page } = await useAsyncData(route.path, () => {
 	return queryCollection('pages').path(route.path).first();
 });
+
+if (!page.value) {
+	throw createError({
+		statusCode: 404,
+		statusMessage: 'Page Not Found',
+	});
+}
 </script>

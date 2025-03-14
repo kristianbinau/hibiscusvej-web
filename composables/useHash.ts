f4643a5efcc494d55b90911c905e6ac02d281aec
Kristian Binau <kristian@binau.me>
2025-03-14 21:28:23 +0100

    Update useHash to work with ohash@2

--- composables/useHash.ts
+++ composables/useHash.ts
@@ -1,9 +1,37 @@
-import { sha256 } from 'ohash';
+import { digest } from 'ohash';
 
 export const useHash = () => {
 	async function hash(message: string) {
-		return sha256(message);
+		return base64UrlToHex(digest(message));
 	}
 
 	return { hash };
 };
+
+function base64UrlToHex(base64UrlString: string): string {
+	// 1. Prepare for standard Base64 decoding (atob)
+	// Base64Url uses '-' instead of '+', '_' instead of '/', and removes padding '='
+	let base64String = base64UrlString.replace(/-/g, '+').replace(/_/g, '/');
+
+	// Add padding if necessary. Base64 strings should have a length that is a multiple of 4
+	switch (base64String.length % 4) {
+		case 2:
+			base64String += '==';
+			break;
+		case 3:
+			base64String += '=';
+			break;
+	}
+
+	// 2. Decode the Base64 string to a binary string
+	const binaryString = atob(base64String);
+
+	// 3. Convert the binary string to Hex
+	let hexString = '';
+	for (let i = 0; i < binaryString.length; i++) {
+		const hexByte = binaryString.charCodeAt(i).toString(16).padStart(2, '0');
+		hexString += hexByte;
+	}
+
+	return hexString;
+}
